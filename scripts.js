2241f001af9117dcd26f9e4c7971b8b2f439ac4e
Noyon-Saudagor <saudagornoyon4@gmail.com>
2026-03-13 02:15:02 +0600

    add download btn functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const filters = {
+let filters = {
   brightness: {
     value: 100,
     min: 0,
@@ -11,12 +11,7 @@ const filters = {
     max: 200,
     unit: "%",
   },
-  exposure: {
-    value: 100,
-    min: 0,
-    max: 200,
-    unit: "%",
-  },
+  
   satruration: {
     value: 100,
     min: 0,
@@ -30,7 +25,7 @@ const filters = {
     unit: "deg",
   },
   blur: {
-    value: 100,
+    value: 0,
     min: 0,
     max: 20,
     unit: "px",
@@ -61,6 +56,16 @@ const filters = {
   },
 };
 
+const imageCanvas = document.querySelector("#image-canvas")
+const imageInput = document.querySelector("#image-input")
+const canvasCtx = imageCanvas.getContext("2d")
+const resetBtn = document.querySelector("#reset-btn")
+const downloadBtn = document.querySelector("#download-btn")
+
+
+let file = null
+let image = null
+
 const filtersContainer = document.querySelector(".filters")
 
 function createFilterElement(name, unit = "%", value, min, max) {
@@ -79,10 +84,17 @@ function createFilterElement(name, unit = "%", value, min, max) {
   div.appendChild(p);
   div.appendChild(input);
 
+  input.addEventListener("input", (event) =>{
+    filters[name].value = input.value;
+    applyFilters();
+    
+  })
+
   return div;
 }
 
-Object.keys(filters).forEach((key) => {
+function createFilters (){
+  Object.keys(filters).forEach((key) => {
   const filterElement = createFilterElement(key,
     filters[key].unit,
     filters[key].value,
@@ -91,3 +103,111 @@ Object.keys(filters).forEach((key) => {
   );
   filtersContainer.appendChild(filterElement)
 });
+}
+
+createFilters()
+
+imageInput.addEventListener("change", (event) => {
+  file = event.target.files[0]
+  const imagePlaceHolder = document.querySelector(".placeholder")
+  imageCanvas.style.display = "block"
+  imagePlaceHolder.style.display = "none"
+
+  const img = new Image()
+  img.src = URL.createObjectURL(file)
+
+  img.onload = () => {
+    image = img
+    imageCanvas.width = img.width;
+    imageCanvas.height = img.height;
+    canvasCtx.drawImage(img, 0, 0)
+  }
+
+})
+
+function applyFilters(){
+  canvasCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height)
+  canvasCtx.filter =`
+  brightness(${filters.brightness.value}${filters.brightness.unit})
+  contrast(${filters.contrast.value}${filters.contrast.unit})
+  saturate(${filters.satruration.value}${filters.satruration.unit})
+  hue-rotate(${filters.hueRotation.value}${filters.hueRotation.unit})
+  blur(${filters.blur.value}${filters.blur.unit})
+  grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+  sepia(${filters.sepia.value}${filters.sepia.unit})
+  opacity(${filters.opacity.value}${filters.opacity.unit})
+  invert(${filters.invert.value}${filters.invert.unit})
+`.trim()
+  canvasCtx.drawImage(image, 0, 0)
+
+}
+
+resetBtn.addEventListener("click", ()=>{
+    filters = {
+  brightness: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  contrast: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  
+  satruration: {
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: "%",
+  },
+  hueRotation: {
+    value: 0,
+    min: 0,
+    max: 360,
+    unit: "deg",
+  },
+  blur: {
+    value: 0,
+    min: 0,
+    max: 20,
+    unit: "px",
+  },
+  grayscale: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
+  },
+  sepia: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
+  },
+  opacity: {
+    value: 100,
+    min: 0,
+    max: 100,
+    unit: "%",
+  },
+  invert: {
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: "%",
+  },
+};
+applyFilters()
+filtersContainer.innerHTML = ""
+createFilters()
+})
+
+downloadBtn.addEventListener("click", () => {
+    const link = document.createElement("a")
+    link.download = "edited-image.png"
+    link.href = imageCanvas.toDataURL()
+    link.click()
+})
